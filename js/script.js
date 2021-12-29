5fec9f335765af9aae86f7ab953c9a96fb8a9ff5
sanya <sanyaputilov13121996@gmail.com>
2021-12-29 15:00:11 +0300

    Начальная работа с приложением

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
- console.log(1+1);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просморенных фильмов?', ''),
+      b = prompt('На сколько его оцените?', ''),
+      c = prompt('Один из последних просморенных фильмов?', ''),
+      d = prompt('На сколько его оцените?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
