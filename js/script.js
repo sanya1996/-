78e81ed785b1215d2309372288154fdfcf6b62fb
sanya <sanyaputilov13121996@gmail.com>
2022-01-12 11:46:58 +0300

    практика часть 2

--- js/script.js
+++ js/script.js
@@ -8,12 +8,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просморенных фильмов?', ''),
-      b = prompt('На сколько его оцените?', ''),
-      c = prompt('Один из последних просморенных фильмов?', ''),
-      d = prompt('На сколько его оцените?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+for (let i = 0; i<2; i++) {
+    const a = prompt('Один из последних просморенных фильмов?',''),
+          b = prompt('На сколько его оцените?','');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a]=b;
+            console.log('done');
+        } else {
+            console.log('err');
+            i--;
+        }
+}
+
+if (+numberOfFilms < 10){
+    alert('Просмотренно довольно мало фильмов');
+} else if (+numberOfFilms >= 10 && +numberOfFilms < 30) {
+    alert('Вы классический зритель');
+} else if (+numberOfFilms >= 30) {
+    alert ('Вы киноман!');
+} else {
+    alert ('Произошла ошибка');
+}
+   
 
 console.log(personalMovieDB);
+
+
+(numberOfFilms < 10) ? alert('Просмотренно довольно мало фильмов') :
+(numberOfFilms >= 10 && numberOfFilms < 30) ? alert('Вы классический зритель') : 
+(numberOfFilms >= 30) ? alert ('Вы киноман!') : console.log('err');
