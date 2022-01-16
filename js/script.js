6d0ac01eff728a59147418710ea3113d410076a4
sanya <sanyaputilov13121996@gmail.com>
2022-01-16 20:03:35 +0300

    Работа с объектами

--- js/script.js
+++ js/script.js
@@ -1,72 +1,72 @@
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == ''|| numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: () => {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == ''|| personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyLilms:() => {
+        for (let i = 0; i<2; i++) {
+            const a = prompt('Один из последних просморенных фильмов?',''),
+                  b = prompt('На сколько его оцените?','');
+        
+                if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                    personalMovieDB.movies[a]=b;
+                    console.log('done');
+                } else {
+                    console.log('err');
+                    i--;
+                }
+        }
+    },
+    detectPersinalLevel:() => {
+        if (+numberOfFilms < 10){
+            console.log('Просмотренно довольно мало фильмов');
+        } else if (+numberOfFilms >= 10 && +numberOfFilms < 30) {
+            console.log('Вы классический зритель');
+        } else if (+numberOfFilms >= 30) {
+            console.log('Вы киноман!');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB:() => {
+        while (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+            break;
+        }
+    },
+    writeYourgenres:() => {
+        for (let i = 0; i<3; i++) {
+            const a = prompt(`Ваш любимый жанр под номером ${personalMovieDB.genres.length +1}?`,'');
+            if (a == ''|| a == null) {
+                i--;
+            }else{
+                personalMovieDB.genres.push(a);
+            }
+            
+        }     
+        personalMovieDB.genres.forEach((item, i) =>  {
+                console.log(`Любимый жанр ${i+1} это ${item}`);
+            });
+        
+    },
+    toggleVisiblemyDB:() => {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    }
+
 };
 
-
-function rememberMyLilms() {
-    for (let i = 0; i<2; i++) {
-        const a = prompt('Один из последних просморенных фильмов?',''),
-              b = prompt('На сколько его оцените?','');
-    
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a]=b;
-                console.log('done');
-            } else {
-                console.log('err');
-                i--;
-            }
-    }
-}
-
-// rememberMyLilms();
-
-function detectPersinalLevel() {
-    if (+numberOfFilms < 10){
-        console.log('Просмотренно довольно мало фильмов');
-    } else if (+numberOfFilms >= 10 && +numberOfFilms < 30) {
-        console.log('Вы классический зритель');
-    } else if (+numberOfFilms >= 30) {
-        console.log('Вы киноман!');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-
-// detectPersinalLevel();
-
-function showMyDB() {
-    while (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-        break;
-    }
-}
-
-// showMyDB();
-
-function writeYourgenres() {
-    for (let i = 0; i<3; i++) {
-        const a = prompt(`Ваш любимый жанр под номером ${personalMovieDB.genres.length +1}?`,'');
-        personalMovieDB.genres.push(a);
-    }
-}
-
-writeYourgenres();
 
 console.log(personalMovieDB);
 
