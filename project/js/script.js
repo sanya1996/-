19d631efeba57c38af37224697c51d0e9c04df56
sanya <sanyaputilov13121996@gmail.com>
2022-02-12 11:03:49 +0300

    конец практике

--- project/js/script.js
+++ project/js/script.js
@@ -1,83 +1,89 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
-
-const movieDB = {
-    movies: [
-        "Воин",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const spam = document.querySelectorAll('.promo__adv img'),
-      promo = document.querySelector('.promo__bg'),
-      genre = promo.querySelector('.promo__genre'),
-      films = document.querySelector('.promo__interactive-list'),
-      remove = document.querySelector('.add'),
-      imput = remove.querySelector('.adding__input'),
-      checkbox = imput.nextElementSibling.nextElementSibling;
-      
-
-spam.forEach( (e) => {
-    e.remove();
-});
-
-genre.textContent = 'ДРАМА';
-
-promo.style.backgroundImage = 'url("img/bg.jpg")';
-
-remove.addEventListener ('submit',(e) => {
-    e.preventDefault();
-    let newFilm = imput.value;
-
-    if(newFilm) {
-        if (newFilm.length > 21){
-            newFilm = `${newFilm.substr(0,22)}...`;
-        }
-        movieDB.movies.push(newFilm);
-        movieDB.movies.sort();
-        cal();
-        imput.value = "";
-    }
-
-     
-});
-
-function cal () {
-    films.innerHTML = "";
-    movieDB.movies.forEach( (film, i) => {
-        films.innerHTML += `
-         <li class="promo__interactive-item">${i+1}.${film}
-                        <div class="delete"></div>
-        </li>
-    `;
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Воин",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
     
-}
-cal(1);
- 
-checkbox.addEventListener('click', (e)=>{
-    e = checkbox.checked;
-    if (e == true){
-        console.log('Добавляем любимый фильм');
+    const spam = document.querySelectorAll('.promo__adv img'),
+          promo = document.querySelector('.promo__bg'),
+          genre = promo.querySelector('.promo__genre'),
+          films = document.querySelector('.promo__interactive-list'),
+          remove = document.querySelector('.add'),
+          imput = remove.querySelector('.adding__input'),
+          checkbox = imput.nextElementSibling.nextElementSibling;
+    
+    remove.addEventListener ('submit',(e) => {
+        e.preventDefault();
+        let newFilm = imput.value;
+        let fav = checkbox.checked;
+
+        if(newFilm) {
+
+            if (newFilm.length > 21){
+                newFilm = `${newFilm.substring(0,22)}...`;
+                }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+
+            cal(movieDB.movies, films);
+
+            if (fav){
+                console.log('Добавляем любимый фильм');
+            } 
+        }  
+        e.target.reset();
+    });
+
+    const delArr = (arr) => {
+        arr.forEach( (e) => {
+            e.remove();
+        });
+    };
+    
+    const makeChanges = () => {
+        genre.textContent = 'ДРАМА';
+        promo.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+    
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    
+    
+    
+    function cal (movies, parent) {
+        parent.innerHTML = "";
+
+        sortArr(movies);
+
+        movies.forEach( (film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i+1}.${film}
+                            <div class="delete"></div>
+                </li>
+        `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn,i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                cal(movies, parent);
+            });
+        });
     }
+    
+     
+    delArr(spam);
+    makeChanges();
+    cal(movieDB.movies, films);
+    
+    
 });
-
-
